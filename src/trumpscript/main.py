c39baa8fc5de12afb707e3368e9ef46c0666f5e8
Nik Nyby <nnyby@columbia.edu>
2016-01-22 10:10:51 -0500

    Set default encoding to ascii
    Python 3 defaults encoding to 'utf-8', however, writing programs in languages other than English conflicts with the ideals of TrumpScript.

--- src/trumpscript/main.py
+++ src/trumpscript/main.py
@@ -8,6 +8,10 @@ __author__ = 'github.com/samshadwell'
 
 
 def main():
+    # TrumpScript only runs in ascii mode. If you don't speak English
+    # you can't write TrumpScript.
+    sys.setdefaultencoding('ascii')
+
     if len(sys.argv) != 2:
         print("Invalid usage. Provide a TrumpScript file name to compile and run")
         print("Example: TRUMP trump_file.tr")
